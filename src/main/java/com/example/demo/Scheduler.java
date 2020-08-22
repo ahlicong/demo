67d9fd774ecca85fe0359224b4e1e82297567634
LC <licong75@qq.com>
2020-08-22 16:53:01 +0800

    date format

--- src/main/java/com/example/demo/Scheduler.java
+++ src/main/java/com/example/demo/Scheduler.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Scheduler {
 	private static final Logger logger=LoggerFactory.getLogger(Scheduler.class);
 	
@@ -14,7 +16,7 @@ public class Scheduler {
 	/**
 	 * trigger at 0:00:00 everyday
 	 */
-	@Scheduled(cron="0 0 0 * * ? *")
+	@Scheduled(cron="0 0 0 * * ?")
 	public void refreshRanks() {
 		logger.info("refresh ranks start.");
 		dao.refreshRanks();
